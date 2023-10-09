fbc723466c0c8b2137f2d51374b14bf7c5de8fd7
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-10-08 23:44:28 -0300

    Projeto inicial

---
+++ mysalles/mysalles/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mysalles.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
