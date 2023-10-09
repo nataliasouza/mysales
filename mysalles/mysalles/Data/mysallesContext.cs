940eb4aa21c147eba70ab7af9ea5de95d8a28954
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-10-09 18:00:06 -0300

    View - Departments e DepartmentsController

---
+++ mysalles/mysalles/Data/mysallesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using mysalles.Models;
+
+namespace mysalles.Data
+{
+    public class mysallesContext : DbContext
+    {
+        public mysallesContext (DbContextOptions<mysallesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<mysalles.Models.Department> Department { get; set; }
+    }
+}
