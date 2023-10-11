dba6478bd7c58192d4ec87e4b3341cc4f04c6d77
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-10-11 14:37:50 -0300

    SellersController

---
+++ mysalles/mysalles/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mysalles.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
