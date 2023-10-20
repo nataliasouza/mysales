5ddd4324c492a615a50945f5940bd8ec04a33163
Natalia Souza <nataliasouza.ti@hotmail.com>
2023-10-19 21:02:41 -0300

    I adjusted the name of the context class

---
+++ mysalles/mysalles/Data/MySalesContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mySales.Models;
+
+namespace mySales.Data
+{
+    public class MySalesContext : DbContext
+    {
+        public MySalesContext (DbContextOptions<MySalesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller {  get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+    }
+}
